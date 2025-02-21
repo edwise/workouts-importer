cbd4939c30f8d50c11dc7c3f408c64b5f5e97ec0
edwise <eantonc@gmail.com>
2025-02-21 13:44:37 +0100

    Initial commit

---
+++ src/main/java/com/edwise/batch/workoutsimporter/WorkoutsImporterApplication.java
@@ -0,0 +1,13 @@
+package com.edwise.batch.workoutsimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutsImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutsImporterApplication.class, args);
+	}
+
+}
