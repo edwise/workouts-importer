79c0aa25bdcea50b0998c4042e336fab14d95762
edwise <eantonc@gmail.com>
2025-03-03 13:05:27 +0100

    test: change test to check something of the inserted values

--- src/test/java/com/edwise/batch/workoutsimporter/WorkoutsImporterApplicationTests.java
+++ src/test/java/com/edwise/batch/workoutsimporter/WorkoutsImporterApplicationTests.java
@@ -1,7 +1,7 @@
 package com.edwise.batch.workoutsimporter;
 
+import com.edwise.batch.workoutsimporter.model.Workout;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.ExitStatus;
@@ -12,10 +12,15 @@ import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
@@ -33,19 +38,37 @@ class WorkoutsImporterApplicationTests {
     @Autowired
     private Job workoutsImporterJob;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     @BeforeEach
     void setUp() {
         jobLauncherTestUtils.setJob(workoutsImporterJob);
+        mongoTemplate.dropCollection(Workout.class);
     }
 
     @AfterEach
     void tearDown() {
         jobRepositoryTestUtils.removeJobExecutions();
+        mongoTemplate.dropCollection(Workout.class);
     }
+
 
     @Test
     void shouldEndCompletedWorkoutImporterJob() throws Exception {
         JobExecution jobExecution = jobLauncherTestUtils.launchJob();
-        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+        assertThat(jobExecution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED);
+
+        List<Workout> workouts = mongoTemplate.findAll(Workout.class);
+        assertThat(workouts)
+                .isNotEmpty()
+                .hasSize(6);
+        assertThat(workouts)
+                .anySatisfy(workout -> {
+                    assertThat(workout.getTitle()).isEqualTo("Rutina Test");
+                    assertThat(workout.getExerciseTitle()).isEqualTo("Bench Press (Barbell)");
+                    assertThat(workout.getWeightKg()).isEqualTo(40.0);
+                    assertThat(workout.getReps()).isEqualTo(10);
+                });
     }
 }
