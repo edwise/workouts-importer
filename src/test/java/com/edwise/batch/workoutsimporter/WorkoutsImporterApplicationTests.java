6ba14269683315be82bbabede55213b1efb94d5c
edwise <eantonc@gmail.com>
2025-03-10 13:36:39 +0100

    feat: add tests containers to avoid having a mongo instance working

--- src/test/java/com/edwise/batch/workoutsimporter/WorkoutsImporterApplicationTests.java
+++ src/test/java/com/edwise/batch/workoutsimporter/WorkoutsImporterApplicationTests.java
@@ -2,6 +2,7 @@ package com.edwise.batch.workoutsimporter;
 
 import com.edwise.batch.workoutsimporter.model.Workout;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.ExitStatus;
@@ -14,6 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 import java.util.List;
 
@@ -23,6 +28,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBatchTest
 @SpringBootTest
 class WorkoutsImporterApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0");
 
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
@@ -36,18 +44,25 @@ class WorkoutsImporterApplicationTests {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @BeforeAll
+    static void setupContainer() {
+        mongoDBContainer.start();
+    }
+
     @BeforeEach
     void setUp() {
         jobLauncherTestUtils.setJob(workoutsImporterJob);
-        mongoTemplate.dropCollection(Workout.class);
     }
 
     @AfterEach
     void tearDown() {
         jobRepositoryTestUtils.removeJobExecutions();
-        mongoTemplate.dropCollection(Workout.class);
     }
-
 
     @Test
     void shouldEndCompletedWorkoutImporterJob() throws Exception {
